423c75124d35efe556f842d469f30b5526f9a564
shadowmind1337 <shadowmind1337@gmail.com>
2026-06-15 06:39:00 +0000

    Deploy build to static repo with SPA fix

---
+++ assets/LoginPage-YPHhClhj.js
@@ -0,0 +1,1 @@
+import{p as r}from"./vendor-base-Dc6_k_WZ.js";import{n as o}from"./index-DajohxU7.js";var i=r();function m(){return(0,i.jsx)(o,{})}export{m as default};
---
+++ assets/RegisterPage-DDSnWSIH.js
@@ -0,0 +1,1 @@
+import{p as r}from"./vendor-base-Dc6_k_WZ.js";import{t as e}from"./index-DajohxU7.js";var t=r();function o(){return(0,t.jsx)(e,{})}export{o as default};
